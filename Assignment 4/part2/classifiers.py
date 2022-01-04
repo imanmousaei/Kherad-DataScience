d88938b5901321223a175dd850f38df7a7a6deaf
Iman Mousaei <imanmousaei@sotoon.ir>
2022-01-04 23:01:08 +0330

    Report models accuracy

--- Assignment 4/part2/classifiers.py
+++ Assignment 4/part2/classifiers.py
@@ -1,5 +1,5 @@
 from sklearn.datasets import load_iris
-from sklearn import linear_model, svm, neighbors, naive_bayes, tree, ensemble, neural_network
+from sklearn import linear_model, svm, neighbors, naive_bayes, tree, ensemble, neural_network, metrics
 
 
 class ClassificationModel:
@@ -13,48 +13,62 @@ class ClassificationModel:
     def predict(self, testX):
         return self.classifier.predict(testX)
 
+    def report(self, testX, trueY):
+        self.train()
+        predictY = self.predict(testX)
+
+        # target_names = ['class 0', 'class 1']
+        return metrics.classification_report(trueY, predictY)
+
 
 class LogisticRegression(ClassificationModel):
     def __init__(self, trainX, trainY):
-        super(LogisticRegression).__init__(trainX, trainY)
+        super().__init__(trainX, trainY)
+        self.name = "LogisticRegression"
         self.classifier = linear_model.LogisticRegression(random_state=0)
 
 
 class SVM(ClassificationModel):
     def __init__(self, trainX, trainY):
-        super(SVM).__init__(trainX, trainY)
+        super().__init__(trainX, trainY)
+        self.name = "SVM"
         self.classifier = svm.SVC(kernel="linear", C=0.025, random_state=101)
 
 
 class KNN(ClassificationModel):
     def __init__(self, trainX, trainY):
-        super(KNN).__init__(trainX, trainY)
+        super().__init__(trainX, trainY)
+        self.name = "KNN"
         self.classifier = neighbors.KNeighborsClassifier(n_neighbors=15)
 
 
 class NaiveBayes(ClassificationModel):
     def __init__(self, trainX, trainY):
-        super(NaiveBayes).__init__(trainX, trainY)
+        super().__init__(trainX, trainY)
+        self.name = "NaiveBayes"
         self.classifier = naive_bayes.GaussianNB()
 
 
 class DecisionTree(ClassificationModel):
     def __init__(self, trainX, trainY):
-        super(DecisionTree).__init__(trainX, trainY)
+        super().__init__(trainX, trainY)
+        self.name = "DecisionTree"
         self.classifier = tree.DecisionTreeClassifier(
             max_depth=10, random_state=101, min_samples_leaf=15, max_features=None)
 
 
 class RandomForest(ClassificationModel):
     def __init__(self, trainX, trainY):
-        super(RandomForest).__init__(trainX, trainY)
+        super().__init__(trainX, trainY)
+        self.name = "RandomForest"
         self.classifier = ensemble.RandomForestClassifier(
             n_estimators=7, oob_score=True, random_state=101, max_features=None, min_samples_leaf=30)
 
 
 class NeuralNetwork(ClassificationModel):
     def __init__(self, trainX, trainY):
-        super(NeuralNetwork).__init__(trainX, trainY)
+        super().__init__(trainX, trainY)
+        self.name = "NeuralNetwork"
         self.classifier = neural_network.MLPClassifier(
             solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
 
@@ -62,5 +76,6 @@ class NeuralNetwork(ClassificationModel):
 # todo
 class Transformer(ClassificationModel):
     def __init__(self, trainX, trainY):
-        super(Transformer).__init__(trainX, trainY)
-        self.classifier = neighbors.KNeighborsClassifier(n_neighbors=15)
+        super().__init__(trainX, trainY)
+        self.name = "Transformer"
+        self.classifier = neighbors.KNeighborsClassifier(n_neighbors=25)
--- Assignment 4/part2/main.py
+++ Assignment 4/part2/main.py
@@ -1,45 +1,67 @@
 import pandas as pd
 import random
+from sklearn.model_selection import train_test_split
+import classifiers
+from imblearn.under_sampling import RandomUnderSampler
+from csv import reader
 
 
 train_path = "dataset/train.csv"
 test_path = "dataset/test.csv"
-maxn = int(1e1)
+maxn = int(1e3)
 p = 0.001  # select 0.1% of the lines
 
 
-def read_datasets():
+def read_dataset():
     # since my ram cant load 7GB of data (!!!) we are going we read p% of the rows of it.
     # if random from [0,1] interval is greater than 0.01 the row will be skipped.
-    train = pd.read_csv(
+    dataset = pd.read_csv(
         train_path,
         header=0,
-        chunksize=maxn,
+        nrows=maxn,
         # skiprows=lambda i: i > 0 and random.random() > p
     )
-    # train = train.sample(maxn)
-    train = pd.DataFrame(train.get_chunk(maxn))
+    # dataset = dataset.sample(maxn)
+    # dataset = pd.DataFrame(dataset.get_chunk(maxn))
 
-    trainY = train.loc[:, 'is_attributed']
-    trainX = train.drop(['is_attributed'], 'columns')
-    
+    return dataset
 
-    test = pd.read_csv(
-        test_path,
-        header=0,
-        chunksize=maxn,
-        # skiprows=lambda i: i > 0 and random.random() > p
-    )
-    # test = test.sample(maxn)
-    test = pd.DataFrame(test.get_chunk(maxn))
 
-    testY = test.loc[:, 'is_attributed']
-    testX = test.drop(['is_attributed'], 'columns')
+def split_dataset(dataset: pd.DataFrame):
+    # drop useless columns
+    # attributed_time has data leak to test set so we remove it
+    dataset.drop(['click_time', 'attributed_time'], axis='columns', inplace=True)
 
-    return trainX, trainY, testX, testY
+    datasetY = dataset.loc[:, 'is_attributed']
+    datasetX = dataset.drop(['is_attributed'], 'columns')
+
+    # since test set doesnt have is_attributed column(!) we split train set into train and set. 
+    # because we want to calculate model's accuracy.
+    trainX, testX, trainY, testY = train_test_split(datasetX, datasetY, test_size=0.2, random_state=42)    
+
+    print(trainY)
+
+    return trainX, testX, trainY, testY
+
 
 def main():
-    read_datasets()
+    dataset = read_dataset()
+    trainX, testX, trainY, testY = split_dataset(dataset)
+
+    models = [
+        classifiers.LogisticRegression(trainX, trainY),
+        classifiers.SVM(trainX, trainY),
+        classifiers.KNN(trainX, trainY),
+        classifiers.NaiveBayes(trainX, trainY),
+        classifiers.DecisionTree(trainX, trainY),
+        classifiers.RandomForest(trainX, trainY),
+        classifiers.NeuralNetwork(trainX, trainY),
+        classifiers.Transformer(trainX, trainY),
+    ]
+
+    for model in models:
+        report = model.report(testX, testY)
+        print(model.name,report)
 
 
 if __name__ == "__main__":
