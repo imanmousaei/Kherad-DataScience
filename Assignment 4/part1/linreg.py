6ee3df2f11d8c449eda5797e69441ef902296ae9
Iman Mousaei <imanmousaei@sotoon.ir>
2022-01-07 12:21:16 +0330

    Add part1

--- Assignment 4/part1/linreg.py
+++ Assignment 4/part1/linreg.py
@@ -4,8 +4,8 @@ import numpy as np
 class LinearRegression:
 
     def __init__(self, init_theta=None, alpha=0.01, n_iter=100):
-        self.alpha = alpha
-        self.n_iter = n_iter
+        self.alpha = alpha  # learning rate
+        self.n_iter = n_iter  # number of iterations to hepefully converge
         self.theta = init_theta
         self.JHist = None
 
@@ -19,12 +19,15 @@ class LinearRegression:
         Returns:
             the final theta found by gradient descent
         '''
-        n,d = X.shape
+        # n,d = X.shape
+        n = len(y)
         self.JHist = []
         for i in range(self.n_iter):
+            prediction = self.predict(X, theta)
+            theta = theta -(1/n)*self.alpha*( X.T.dot((prediction - y)))
+
             self.JHist.append((self.computeCost(X, y, theta), theta))
             print("Iteration: ", i+1, " Cost: ", self.JHist[i][0], " Theta: ", theta)
-            # TODO:  add update equation here
             
         return theta
 
@@ -39,7 +42,10 @@ class LinearRegression:
           a scalar value of the cost  
               ** make certain you don't return a matrix with just one value! **
         '''
-        # TODO: add objective (cost) equation here
+        n = len(y)
+        predictions = self.predict(X, theta)
+        cost = (1/2*n) * np.sum(np.square(predictions-y))
+        return cost
 
 
     def fit(self, X, y):
@@ -49,13 +55,13 @@ class LinearRegression:
             X is a n-by-d numpy matrix
             y is an n-dimensional numpy vector
         '''
-        n = len(y)
+        # n = len(y)
         n,d = X.shape
         if self.theta is None:
             self.theta = np.matrix(np.zeros((d,1)))
         self.theta = self.gradientDescent(X,y,self.theta)
 
-    def predict(self, X):
+    def predict(self, X, theta):
         '''
         Used the model to predict values for each instance in X
         Arguments:
@@ -63,4 +69,4 @@ class LinearRegression:
         Returns:
             an n-dimensional numpy vector of the predictions
         '''
-        # TODO:  add prediction function here
+        return np.dot(X,theta)
